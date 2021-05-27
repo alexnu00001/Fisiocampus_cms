af56c151e97e2515590c0181d81cddb2d796a1b3
alex00001 <alex.iv.nu@gmail.com>
2021-05-26 20:37:16 -0500

    add script

--- resources/views/layouts/vuexy/scripts.blade.php
+++ resources/views/layouts/vuexy/scripts.blade.php
@@ -35,13 +35,13 @@
 <script src="{{asset('vuexy/app-assets/vendors/js/pickers/flatpickr/flatpickr.min.js')}}"></script>
 <script src="{{asset('vuexy/app-assets/js/scripts/forms/form-validation.js')}}"></script>
 
-
+<script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/pdfmake.min.js')}}"></script>
  
  
 {{-- <script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/datatables.checkboxes.min.js')}}"></script>
 <script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/jszip.min.js')}}"></script>
 
-<script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/pdfmake.min.js')}}"></script>
+
 <script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/vfs_fonts.js')}}"></script>
 <script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/buttons.html5.min.js')}}"></script>
 <script src="{{asset('vuexy/app-assets/vendors/js/tables/datatable/buttons.print.min.js')}}"></script>
