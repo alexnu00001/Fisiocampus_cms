cee350f0a83e4c9b3cf3e7d153aad6f007d8b46e
alex00001 <alex.iv.nu@gmail.com>
2021-05-26 19:03:42 -0500

    template

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.vuexy')
 @section('content')
-
+<div class="card-datatable table-responsive pt-0">
+    <table class="user-list-table table">
+        <thead class="thead-light">
+            <tr>
+                <th></th>
+                <th>User</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Plan</th>
+                <th>Status</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+    </table>
+</div>
 @endsection 
 @section('scripts')
     @parent
- 
-
+<script>
+</script>
 @endsection
