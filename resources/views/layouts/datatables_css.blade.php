0ee29f4561a3efbef38fef562976487b4a11b706
alex00001 <alex.iv.nu@gmail.com>
2021-05-27 21:39:43 -0500

    Adapted

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,4 +1,8 @@
-<!-- DataTable Bootstrap CSS-->
+{{-- <!-- DataTable Bootstrap CSS-->
 <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.3.1/css/buttons.bootstrap.min.css">
-<!-- / DataTable Bootstrap CSS-->
+<!-- / DataTable Bootstrap CSS--> --}}
+<link rel="stylesheet" type="text/css" href="{{asset('vuexy/app-assets/vendors/css/tables/datatable/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vuexy/app-assets/vendors/css/tables/datatable/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vuexy/app-assets/vendors/css/tables/datatable/buttons.bootstrap4.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vuexy/app-assets/vendors/css/tables/datatable/rowGroup.bootstrap4.min.css')}}">
